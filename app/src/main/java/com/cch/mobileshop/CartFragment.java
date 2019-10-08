1a3ea740d91d5209ddc2174054af294f1147ce0c
huatec-1 <57xiaoyu@163.com>
2019-10-08 09:26:56 +0800

    这是第二次修改，实在宿舍修改的

--- app/src/main/java/com/cch/mobileshop/CartFragment.java
+++ app/src/main/java/com/cch/mobileshop/CartFragment.java
@@ -13,6 +13,7 @@ public class CartFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cart, container, false);
+        //修改
         return view;
     }
 }
